f6e66c4417968835698a689459e36afc247e1d44
Onno Eberhard <OnnoEberhard@gmail.com>
2018-01-09 22:30:28 +0100

    v0.9.1 pprint2.py is the first module of scitools for python 3. I just futurized it, but I will probably do more.

--- examples/demo_pyreport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-scitools pyreport -l -e -t pdf  -o report_mpl -a '0.05 9' demo_pyreport.py
-scitools pyreport -l -e -t html -o report_mpl -a '0.05 9' demo_pyreport.py
-scitools pyreport -l -e -t pdf  -o report_evz -a '0.05 9 easyviz' demo_pyreport.py
-scitools pyreport -l -e -t html -o report_evz -a '0.05 9 easyviz' demo_pyreport.py
